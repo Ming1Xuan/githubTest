4c2e8facf8c9366df9df8b23e8c05df95c7b91b1
Ming1Xuan <102127451+Ming1Xuan@users.noreply.github.com>
2022-04-05 04:05:29 +0800

    Add files via upload

---
+++ HelloStudents.java
@@ -0,0 +1,14 @@
+/*
+Purpose: A sample program to print a message
+“Hello Students!” On the screen.
+Author: S.C. Hui
+Date: 12 July 2007
+*/
+
+public class HelloStudents { 					// begin class
+	public static void main( String[] args ) { 		// main method
+		System.out.println( "Hello Students!" ); // statement
+	} 							// end method
+}
+
+
